68fac5cbf2ee2b1a4fb4d249aaf90306f9f3e9d8
IvanSavchuk <isavchuk99@gmail.com>
2020-07-09 19:19:12 +0300

    update

---
+++ build/precache-manifest.3aa6d70df638a20d3779ac3bac1cf594.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23ab15fd916a250eb75cadb290d512d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c633c2493615c0e39479",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5a96338a68ca8add7863",
+    "url": "/static/js/2.cb2e2edb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cb2e2edb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c633c2493615c0e39479",
+    "url": "/static/js/main.9296f596.chunk.js"
+  },
+  {
+    "revision": "378953c0482a70fbc1d7",
+    "url": "/static/js/runtime-main.4e00ff9c.js"
+  }
+]);
